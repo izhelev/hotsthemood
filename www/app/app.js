cfaefeb320094d957881390b4203825f9e19a972
Ivailo Zhelev <Ivailo.zhelev@execview.co.uk>
2014-10-29 22:56:32 +0000

    Implemented a new search and profile section of theapp

--- www/app/app.js
+++ www/app/app.js
@@ -23,119 +23,31 @@ angular.module("hotsthemoodApp", ['ionic'])
 
 .config(function($stateProvider, $urlRouterProvider) {
 	$stateProvider
-		.state('share', {
+		.state('event', {
 			abstract: true,
 			views: {
-				"tab-checkin": {
-					templateUrl: "app/share/share.html"
+				"event-section": {
+					templateUrl: "app/event/event.html"
 				}
 			}
 		})
-
-		.state('share.mood', {
-			url: "/share/mood",
-			templateUrl: "app/share/mood.html"
-		})
-
-		.state('share.location', {
-			url: "/share/{mood}/location",
-			templateUrl: "app/share/location.html"
-		})
-
-		.state('share.message', {
-			url: "/share/{mood}/message",
-			templateUrl: "app/share/message.html"
-		})
-
-		.state('search', {
-			url: "/search",
-			views: {
-				"tab-search": {
-					templateUrl: "app/search/search.html"
-				}
-			}
-		})
-
-		.state('history', {
-			url: "/history",
-			views: {
-				"tab-history": {
-					templateUrl: "app/history/history.html"
-				}
-			}
-		});
+		.state("event.search",
+			{			
+				url: "/event/search",
+				templateUrl: "app/event/search.html"				
+			})
+		.state("event.profile",
+			{			
+				url: "/event/profile",
+				templateUrl: "app/event/profile.html"				
+			});
 
 	// if none of the above states are matched use this as the fallback
-	$urlRouterProvider.otherwise('/share/mood');
+	$urlRouterProvider.otherwise('/event/search');
 })
 
-.controller('LocationController', ['$scope', '$stateParams', '$http', '$ionicLoading', 'shareData', 'locationHelper', 'deviceIdHelper',  'APP_CONFIG',
-	function($scope, $stateParams, $http, $ionicLoading, shareData, locationHelper, deviceIdHelper,app_config) {
-
-	console.log('LocationController');
-	$ionicLoading.show({
-		template: '<i class="icon loadingIndicator ion-looping"></i>'
-	});
-
-	locationHelper.getLatestLocation(function(location) {
-		console.log(location);
-
-		$http.put(app_config.apiUrlBase + '/situation', {Latitude: location.latitude, Longitude: location.longitude})
-				.success(function(data, status, headers, config) {
-					$scope.nearbyLocations = data.locations;
-					$ionicLoading.hide();
-				})
-				.error(function(data, status, headers, config) {
-					$ionicLoading.hide();
-				});
-		});
-
-
-	$scope.selectLocation = function(location) {
-		$ionicLoading.show({
-			template: '<i class="icon loadingIndicator ion-looping"></i>'
-		});
-
-		var deviceId = deviceIdHelper.get();
-
-		var request = {
-			checkin: {
-				deviceId: deviceId,
-				locationReferenceId: location.reference,
-				mood: $stateParams.mood,
-				location: {
-					name: location.name,
-					reference: location.reference,
-					vicinity: location.vicinity,
-					photoUrl: location.photoUrl
-				}
-			}
-		};
-
-		$http.put(app_config.apiUrlBase + '/checkin', request)
-		.success(function(data, status, headers, config) {
-			$ionicLoading.hide();
-		})
-		.error(function(data, status, headers, config) {
-			$ionicLoading.hide();
-		});
-
-		shareData.location = location;
-	};
-
-	$scope.mood = $stateParams.mood;
-}])
-
-
-.controller('MessageController', ['$scope', '$stateParams', 'shareData', function($scope, $stateParams, shareData) {
-	console.log('MessageController');
-	console.log($stateParams);
-	$scope.mood = $stateParams.mood;
-	$scope.location = shareData.location;
-}])
-
-.controller('SearchController', ['$scope', '$stateParams', '$ionicLoading', '$http', 'locationHelper', 'APP_CONFIG',
-	function($scope, $stateParams, $ionicLoading, $http, locationHelper, app_config) {
+.controller('SearchController', ['$scope', '$stateParams', '$ionicLoading', '$http',  'shareData', 'locationHelper', 'APP_CONFIG',
+	function($scope, $stateParams, $ionicLoading, $http, shareData, locationHelper, app_config) {
 	console.log('SearchController');
 	$ionicLoading.show({
 		template: '<i class="icon loadingIndicator ion-looping"></i>'
@@ -146,37 +58,24 @@ angular.module("hotsthemoodApp", ['ionic'])
 
 		console.log(location);
 
-		$http.put(app_config.apiUrlBase + '/happinessquery', {Latitude: location.latitude, Longitude: location.longitude})
+		$http.put(app_config.apiUrlBase + '/search', {Latitude: location.latitude, Longitude: location.longitude})
 		.success(function(data, status, headers, config) {
-			$scope.nearbyLocations = data.locations;
+			$scope.nearbyEvents = data.events;
 			$ionicLoading.hide();
 		})
 		.error(function(data, status, headers, config) {
 			$ionicLoading.hide();
 		});
 	});
+
+	$scope.selectedEvent = function(event) {
+		shareData.event = event;
+	};
 }])
-
-
-.controller('HistoryController', ['$scope', '$stateParams', '$ionicLoading', '$http', 'deviceIdHelper', 'APP_CONFIG',
-	function($scope, $stateParams, $ionicLoading, $http, deviceIdHelper, app_config) {
-	console.log('SearchController');
-	$ionicLoading.show({
-		template: '<i class="icon loadingIndicator ion-looping"></i>'
-	});
-
-	$http.get(app_config.apiUrlBase + '/checkinHistory/' + deviceIdHelper.get())
-		.success(function(data, status, headers, config) {
-			$scope.checkinHistory = [];
-			for(var i = 0; i < data.checkins.length; i++) {
-				var checkin = data.checkins[i];
-				checkin.timestamp = moment(checkin.timestamp).fromNow();
-				$scope.checkinHistory.push(checkin);
-			}
-			$ionicLoading.hide();
-		})
-		.error(function(data, status, headers, config) {
-			$ionicLoading.hide();
-		});
-
+.controller('EventProfileController', ['$scope', '$stateParams', 'shareData', function($scope, $stateParams, shareData) {
+	console.log('EventProfileController');
+	console.log($stateParams);
+	//console.log(shareData.event);
+	$scope.event = shareData.event;
 }]);
+
--- www/app/appConfig.js
+++ www/app/appConfig.js
@@ -1,3 +1,3 @@
 angular.module('hotsthemoodApp').constant('APP_CONFIG', {
-  apiUrlBase: 'http://hotsthemood.apphb.com',
+  apiUrlBase: 'http://localhost:49968',
 });
